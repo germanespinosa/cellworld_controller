7b0b2fb37cc44cc154e2992e3b933ff73875d55d
gabwink <gabriellewink2025@u.northwestern.edu>
2022-02-27 16:15:57 -0600

    deleted distance tolerance added it to robot lib instesd

--- src/pid_controller.cpp
+++ src/pid_controller.cpp
@@ -12,12 +12,6 @@ namespace controller{
             speed = parameters.pursue_speed;
         }
         in = inputs;
-        auto dist = inputs.location.dist(inputs.destination);
-        if (dist <0.06) {
-            out.left = 0;
-            out.right = 0;
-            return out;
-        }
         double destination_theta = inputs.location.atan(inputs.destination);
         auto theta = to_radians(inputs.rotation);
         error = angle_difference(theta, destination_theta) * direction(theta, destination_theta);
