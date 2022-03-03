e855e2590fc2a500a2c33faee076fea8a2979973
gabwink <gabriellewink2025@u.northwestern.edu>
2022-03-03 11:11:32 -0600

    inreased cell transormation size

--- src/controller_service.cpp
+++ src/controller_service.cpp
@@ -142,7 +142,7 @@ namespace controller {
         world.set_occlusions(occlusions_cgb);
         cells = world.create_cell_group();
         paths = Paths(world.create_paths(Resources::from("paths").key("hexagonal").key(occlusions).key("astar").key("robot").get_resource<Path_builder>()));
-        navigability = Location_visibility(cells, world.cell_shape,Transformation(world.cell_transformation.size * 1.25, world.cell_transformation.rotation));
+        navigability = Location_visibility(cells, world.cell_shape,Transformation(world.cell_transformation.size * 1.5, world.cell_transformation.rotation)); // robot size
     }
 
     void Controller_server::join() {
