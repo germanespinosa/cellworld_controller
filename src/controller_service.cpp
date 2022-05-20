d8d61093db3aefe39fa3d82cf3445341a626404c
germanespinosa <germanespinosa@gmail.com>
2022-05-19 19:52:52 -0500

    change grav

--- src/controller_service.cpp
+++ src/controller_service.cpp
@@ -121,9 +121,9 @@ namespace controller {
         return true;
     }
 
-#define goal_weight 0.1
-#define occlusion_weight 0.25 //was 0.0001
-#define decay 6
+#define goal_weight 0
+#define occlusion_weight 0.0001 //was 0.0001
+#define decay 2
 
     cell_world::Location Controller_server::get_next_stop() {
         auto agent_location = tracking_client.agent.step.location;
